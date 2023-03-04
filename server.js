ced25523383611839950124ce25de534f0a60d92
Ibrahemherzallah <116978495+Ibrahemherzallah@users.noreply.github.com>
2023-03-04 04:44:43 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,71 +1,51 @@
 const express = require('express');
 const app = express();
-const pathh = require('path');
+const path = require('path');
 const axios = require('axios');
-let ejs = require('ejs');
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-app.set('views', pathh.join(__dirname , 'views'));
-app.set('views engine', 'ejs');
-
-
-app.listen(3000,()=>{
-    console.log("start");
-    console.log("http://localhost:3000");
+app.listen(3000, () => {
+  console.log("start");
+  console.log("http://localhost:3000");
 })
 
-async function firstPoc() {
+async function pocFun(Pname) {
   const config = {
     method: "get",
-    url: "https://pokeapi.co/api/v2/pokemon/pikachu",
+    url: `https://pokeapi.co/api/v2/pokemon/${Pname}`,
   };
 
   let res = await axios(config);
-  app.get('/' , (req,res) => {
-    res.render('index',{
-       poc : [
-          {name : 'res.data.name'},
-          {order : 'res.data.order'}
-       ]
-    })
+
+  return res;
+}
+
+
+app.get('/' ,async (req,res) =>{
+  let firstP =await pocFun('pikachu');
+  let secondP =await pocFun('bulbasaur');
+  let thirdP =await pocFun('ditto');
+  let fourthP = await pocFun('cosmog');
+  let fifthP = await pocFun('swablu');
+  let sixthP = await pocFun('jigglypuff');
+  let seventhP = await pocFun('charmander');
+  let eightthP = await pocFun('sylveon');
+  let ninthP = await pocFun('jynx');
+
+
+  res.render('index',{title:'Pokemons',
+       f : firstP.data,
+       s : secondP.data,
+       t : thirdP.data ,
+       four :  fourthP.data,  
+       five : fifthP.data,
+       six : sixthP.data,
+       seven : seventhP.data,
+       eight : eightthP.data,
+       nine : ninthP.data
+  })
+   
 })
-
-
-  console.log("The first poc");
-  console.log(`Pokemon Name: ${res.data.name}`);
-  console.log(`Pokemon Order: ${res.data.order}`);
-}
-
-async function secondPoc() {
-    const config2 = {
-      method: "get", 
-      url: "https://pokeapi.co/api/v2/item/251/", 
-    };
-  
-    let res2 = await axios(config2);
-  
-    console.log("The second poc");
-    console.log(`Pokemon Name: ${res2.data.name}`);
-    console.log(`Pokemon Order: ${res2.data.order}`);
-
-
-}
-async function thirdPoc() {
-    const config3 = {
-      method: "get", 
-      url: "https://pokeapi.co/api/v2/pokemon-form/132/", // API link
-    };
-  
-    let res3 = await axios(config3);
-  
-    console.log("The third poc");
-    console.log(`Pokemon Name: ${res3.data.name}`);
-    console.log(`Pokemon Order: ${res3.data.order}`);
-}
-firstPoc();
-secondPoc();
-thirdPoc();
-
-
-
 
